682e74553d93b7bde58a18a6ad0609541f32e51e
Mohammad Aghili <79529016+MhAghili@users.noreply.github.com>
2023-07-07 12:13:56 +0330

    Update ShoppingCart.h
     updated the comments and documentation due to pdf report

--- cpp/src/ShoppingCart.h
+++ cpp/src/ShoppingCart.h
@@ -9,15 +9,39 @@
 #include "Receipt.h"
 #include "SupermarketCatalog.h"
 
+// ShoppingCart
+//Represents a shopping cart that stores the items added by the user.
+
 class ShoppingCart {
 public:
+
+     // Get the list of items in the shopping cart.
+     //A vector of ProductQuantity objects representing the items.
     std::vector<ProductQuantity> getItems() const;
+
+   // Get the quantities of products in the shopping cart.
+
+     // A map with Product as key and double as the quantity.
 
     std::map<Product, double> getProductQuantities() const;
 
+
+    // Add a single item to the shopping cart.
+    // product The product to add.
+
     void addItem(const Product& product);
 
+
+     //Add a specific quantity of an item to the shopping cart.
+    // product The product to add.
+   // quantity The quantity of the product to add.
+
     void addItemQuantity(const Product& product, double quantity);
+
+     // Apply offers to the items in the shopping cart and update the receipt.
+     // receipt The receipt object to update with discounts.
+     // offers A map of offers to be applied.
+   
 
     void handleOffers(Receipt& receipt, std::map<Product, Offer> offers, SupermarketCatalog* catalog);
 
